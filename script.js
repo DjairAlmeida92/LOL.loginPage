747cb3d90d9af69e9424b1572a54fb0fac7e96be
djair <djaptbr10@gmail.com>
2022-10-26 19:20:06 +0100

    third commit to LOL.loginPage

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputs = document.querySelectorAll('.input');
 
+
 const handleFocus = ({ target }) => {
     const span = target.previousElementSibling;
     span.classList.add('span-active');
@@ -12,9 +13,8 @@ const handleFocusOut = ({ target }) => {
     }
 }
 
+
+
 inputs.forEach((input) => input.addEventListener('focus', handleFocus));
 inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
-
-//MINUTO 44 DO VIDEO
-
 
